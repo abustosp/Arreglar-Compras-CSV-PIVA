aed98fed346655153bdc293a74f7b9e4fdf2e0d7
Agustin Bustos Piasentini bustos-agustin@hotmail.com <63634574+abustosp@users.noreply.github.com>
2024-01-18 13:08:42 -0300

    Add script to fix purchases data

---
+++ Arreglar_Compras.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+from tkinter.messagebox import showinfo
+
+# Ruta del archivo Excel con la lista de proveedores
+ruta_proveedores_excel = 'PROVEEDORES.xlsx'
+
+# Leer las columnas ORIGEN y DESTINO de la hoja 1 del archivo Excel
+df_proveedores_Origen_Destino = pd.read_excel(ruta_proveedores_excel, sheet_name='Hoja1', usecols=['CUIT', 'ORIGEN', 'DESTINO'])
+
+df_proveedores_Origen_Destino['ORIGEN'] = df_proveedores_Origen_Destino['ORIGEN'].astype(str)
+df_proveedores_Origen_Destino['DESTINO'] = df_proveedores_Origen_Destino['DESTINO'].astype(str)
+df_proveedores_Origen_Destino['CUIT'] = df_proveedores_Origen_Destino['CUIT'].astype(np.int64)
+
+
+
+# Ruta donde se encuentra al archivo CSV de AFIP
+ruta_completa = 'comprobantes_compras.csv'
+
+# DataFrame con datos originales
+df = pd.read_csv(ruta_completa, sep=';', header=0)
+
+
+
+# si el CUIT de df se encuentra en el CUIT de df_proveedores_Origen_Destino, entonces reemplazar el valor de la columna ORIGEN del df por el de la columna DESTINO del df (ORIGEN y DESTINO se encuentran en df_proveedores_Origen_Destino)
+# Combinar DataFrames en función del CUIT
+df_merged = pd.merge(df, df_proveedores_Origen_Destino, left_on='Nro. Doc. Vendedor', right_on='CUIT', how='left')
+
+df_merged["CUIT"].fillna(0, inplace = True)
+
+for row in range(len(df_merged)):
+    # Asignar valores de las columnas de ORIGEN y DESTINO a variables
+    Origen = df_merged['ORIGEN'][row]
+    Destino = df_merged['DESTINO'][row]
+    Cuit = df_merged['CUIT'][row]
+    # reemplazar el valor de la columna de Destino por el de la columna de Origen cuando el valor de la columna de Destino sea nulo y el valor de la columna de Origen no sea nulo
+    if Cuit != 0:
+        df_merged.loc[row, Destino] = df_merged.loc[row, Origen]
+        df_merged.loc[row, Origen] = np.nan
+
+# Eliminar columnas ORIGEN, DESTINO y CUIT
+df_merged.drop(['ORIGEN', 'DESTINO', 'CUIT'], axis=1, inplace=True)
+
+# Guardar el DataFrame actualizado en un nuevo archivo CSV si es necesario
+df_merged.to_csv('comprobantes_compras_Actualizado.csv', sep=';', index=False , decimal=',')
+
+showinfo('Información', 'El archivo se ha actualizado correctamente')
